6595fb72e101b4a68c8d4f8aeae230e9faaca2bf
Clara Resende Maia <49600181+clarice42@users.noreply.github.com>
2022-07-06 13:20:30 -0300

    ✨Add TileMap header file

---
+++ include/TileMap.hpp
@@ -0,0 +1,31 @@
+#ifndef TILEMAP_HPP
+#define TILEMAP_HPP
+
+#include <iostream>
+#include <vector>
+#include "Component.hpp"
+#include "TileSet.hpp"
+
+using namespace std;
+
+class TileMap : public Component {
+    private:
+    vector<int> tileMatrix;
+    TileSet* tileSet;
+    int mapWidth;
+    int mapHeight;
+    int mapDepth;
+
+    public:
+    TileMap(GameObject&, string, TileSet*);
+    void Load(string);
+    void SetTileSet(TileSet*);
+    int& At(int, int, int z = 0);
+    void Render();
+    void RenderLayer(int, int cameraX = 0, int cameraY = 0);
+    int GetWidth();
+    int GetHeight();
+    int GetDepth();
+};
+
+#endif
