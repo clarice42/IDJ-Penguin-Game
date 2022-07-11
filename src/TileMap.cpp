656f4417c405e9bc9210d06108d1f2d3941a0e7a
Clara Resende Maia <49600181+clarice42@users.noreply.github.com>
2022-07-11 12:46:00 -0300

    ✨🚧 Add a few methods to TimeMap

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -8,3 +8,33 @@ TileMap::TileMap(GameObject& associated, string file, TileSet* tileSet) : Compon
 void TileMap::Load(string file) {
     
 }
+
+void TileMap::SetTileSet(TileSet* tileSetRec) {
+    tileSet = tileSetRec;
+}
+
+int& TileMap::At(int x, int y, int z) {
+    for(vector<int>::size_type i = 0; i < tileMatrix.size(); i++) {
+
+    }
+}
+
+void TileMap::RenderLayer(int layer, int cameraX, int cameraY) {
+
+}
+
+void TileMap::Render() {
+
+}
+
+int TileMap::GetWidth() {
+    return mapWidth;
+}
+
+int TileMap::GetHeight() {
+    return mapHeight;
+}
+
+int TileMap::GetDepth() {
+    return mapDepth;
+}
