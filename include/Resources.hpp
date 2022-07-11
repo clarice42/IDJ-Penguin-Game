9c162b6e02ebcfcbf5d44cdb0f22b853b071fc74
Clara Resende Maia <49600181+clarice42@users.noreply.github.com>
2022-07-11 13:11:58 -0300

    ✨ Add Resources header file

---
+++ include/Resources.hpp
@@ -0,0 +1,24 @@
+#ifndef RESOURCES_HPP
+#define RESOURCES_HPP
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Resources {
+    private:
+    unordered_map<string, SDL_Texture*> imageTable;
+    unordered_map<string, Mix_Music*> musicTable;
+    unordered_map<string, Mix_Chunk*> soundTable;
+
+    public:
+    SDL_Texture* GetImage(string);
+    void ClearImages();
+    Mix_Music* GetMusic(string);
+    void ClearMusics();
+    Mix_Chunk* GetSound(string);
+    void ClearSounds();
+};
+
+#endif
