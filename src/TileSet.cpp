28f5ea54f715e8986a5b3ea1ac0ad3dc52114a40
clarice42 <claramaia820@gmail.com>
2022-07-18 21:57:54 -0300

    🐛 Fix tileset dimensions

--- src/TileSet.cpp
+++ src/TileSet.cpp
@@ -6,16 +6,18 @@ TileSet::TileSet(int width, int height, string file) : tileSet(*new GameObject)
   tileWidth = width;
   tileHeight = height;
   tileSet.Open(file);
-  rows = tileSet.GetWidth() / tileWidth;
-  columns = tileSet.GetHeight() / tileHeight;
+  rows = tileSet.GetHeight() / tileHeight;
+  columns = tileSet.GetWidth() / tileWidth;
 }
 
 void TileSet::RenderTile(unsigned int index, float x, float y)
 {
-  unsigned int numberOfTiles = rows * columns;
+  unsigned int numberOfTiles = (rows * columns) - 1;
   if (index <= numberOfTiles)
   {
-    tileSet.SetClip(x, y, tileWidth, tileHeight);
+    int x_aux = tileWidth * (index % columns);
+    int y_aux = tileHeight * (index / columns);
+    tileSet.SetClip(x_aux, y_aux, tileWidth, tileHeight);
     tileSet.Render(x, y);
   }
 }
