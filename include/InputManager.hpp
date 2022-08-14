658f059838675552268b7d3fdfd59d4e2e33feeb
clarice42 <claramaia820@gmail.com>
2022-08-14 20:08:52 -0300

    ✨ Add Input Manager methods

--- include/InputManager.hpp
+++ include/InputManager.hpp
@@ -2,6 +2,17 @@
 #define INPUT_MANAGER_HPP
 
 #include <iostream>
+#include <unordered_map>
+#define INCLUDE_SDL
+#include "SDL_include.hpp"
+#include <SDL2/SDL.h>
+
+#define LEFT_ARROW_KEY SDLK_LEFT
+#define RIGHT_ARROW_KEY SDLK_RIGHT 
+#define UP_ARROW_KEY SDLK_UP 
+#define DOWN_ARROW_KEY SDLK_DOWN 
+#define ESCAPE_KEY SDLK_ESCAPE 
+#define LEFT_MOUSE_BUTTON SDL_BUTTON_LEFT
 
 using namespace std;
 
@@ -9,6 +20,8 @@ class InputManager {
     private:
     bool mouseState[6];
     int mouseUpdate[6];
+    unordered_map<int, bool> keyState;
+    unordered_map<int, int> keyUpdate;
     bool quitRequested;
     int updateCounter, mouseX, mouseY;
 
@@ -18,6 +31,9 @@ class InputManager {
     void Update();
     bool KeyPress(int);
     bool KeyRelease(int);
+    bool IsKeyDown(int);
+    bool MousePress(int);
+    bool MouseRelease(int);
     bool IsMouseDown(int);
     int GetMouseX();
     int GetMouseY();
--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -1,10 +1,116 @@
 #include "InputManager.hpp"
 
-InputManager& InputManager::GetInstance() {
+InputManager &InputManager::GetInstance()
+{
     static InputManager inputManager;
     return inputManager;
 }
 
-InputManager::InputManager() {
-    
+InputManager::InputManager()
+{
+    mouseState[6] = {};
+    mouseUpdate[6] = {};
+    quitRequested = false;
+    updateCounter = 0;
+    mouseX = 0;
+    mouseY = 0;
 }
+
+void InputManager::Update()
+{
+    SDL_Event event;
+    // Obtenha as coordenadas do mouse
+    SDL_GetMouseState(&mouseX, &mouseY);
+    quitRequested = false;
+    updateCounter++;
+
+    // SDL_PollEvent retorna 1 se encontrar eventos, zero caso contrário
+    while (SDL_PollEvent(&event))
+    {
+
+        // Se o evento for quit, setar a flag para terminação
+        if (event.type == SDL_QUIT)
+        {
+            quitRequested = true;
+        }
+
+        // Se o evento for clique...
+        if (event.type == SDL_MOUSEBUTTONDOWN)
+        {
+            int buttonType = event.button.button;
+            mouseState[buttonType] = true;
+            mouseUpdate[buttonType] = updateCounter;
+        }
+        if (event.type == SDL_MOUSEBUTTONUP)
+        {
+            int buttonType = event.button.button;
+            mouseState[buttonType] = false;
+            mouseUpdate[buttonType] = updateCounter;
+        }
+        if (event.type == SDL_KEYDOWN)
+        {
+            if (!event.key.repeat)
+            {
+                int keyType = event.key.keysym.sym;
+                keyState[keyType] = true;
+                keyUpdate[keyType] = updateCounter;
+            }
+        }
+        if (event.type == SDL_KEYUP)
+        {
+            if (!event.key.repeat)
+            {
+                int keyType = event.key.keysym.sym;
+                keyState[keyType] = false;
+                keyUpdate[keyType] = updateCounter;
+            }
+        }
+    }
+}
+
+bool InputManager::KeyPress(int key)
+{
+    return (updateCounter == keyUpdate[key]) && keyState[key];
+}
+
+bool InputManager::KeyRelease(int key)
+{
+    return (updateCounter == keyUpdate[key]) && !keyState[key];
+}
+
+bool InputManager::IsKeyDown(int key)
+{
+    return keyState[key];
+}
+
+bool InputManager::MousePress(int button)
+{
+    return (updateCounter == mouseUpdate[button]) && mouseState[button];
+}
+
+bool InputManager::MouseRelease(int button)
+{
+    return (updateCounter == mouseUpdate[button]) && !mouseState[button];
+}
+
+bool InputManager::IsMouseDown(int button)
+{
+    return mouseState[button];
+}
+
+int InputManager::GetMouseX()
+{
+    return mouseX;
+}
+
+int InputManager::GetMouseY()
+{
+    return mouseY;
+}
+
+bool InputManager::QuitRequested()
+{
+    return quitRequested;
+}
+
+InputManager::~InputManager() {}
